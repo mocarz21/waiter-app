0db984c893710f2533a38efe4483201c70c7a8e0
Adam Mocarski <adammocarski22@gmail.com>
2022-09-19 00:06:53 +0200

    First commit, add redux files, wireframe

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>hej</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
